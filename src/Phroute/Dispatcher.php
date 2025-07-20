228bb29b607ebaca274f191d3bac3a7c5d6ab680
Kavalar <apuc06@mail.ru>
2025-07-20 15:52:05 +0300

    fix dispatcher

--- src/Phroute/Dispatcher.php
+++ src/Phroute/Dispatcher.php
@@ -55,7 +55,9 @@ class Dispatcher {
         
         $resolvedHandler = $this->handlerResolver->resolve($handler);
 
-        $vars = array_merge($vars, $additionalInfo['vars']);
+        if (isset($additionalInfo['vars'])){
+            $vars = array_merge($vars, $additionalInfo['vars']);
+        }
         $response = call_user_func_array($resolvedHandler, $vars);
 
         return $this->dispatchFilters($afterFilter, $response);
